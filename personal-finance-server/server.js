754157940c05648069b8ab81cb8504e92d0441d8
pnataly <pnataly1992@gmail.com>
2025-04-17 16:21:28 +0300

    add logger

--- personal-finance-server/server.js
+++ personal-finance-server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require('./logger');
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -13,20 +14,20 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+  .then(() => logger.info("MongoDB connected"))
+  .catch((err) => logger.error('Error connecting to MongoDB:', err));
 
-  // set the default connection
 const db = mongoose.connection;
 
-// bind connection to error event - to get notifications of connection errors
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', (err) => logger.error('MongoDB connection error:', err));
 
 db.once('open', function () {
-    console.log('Connected to the database');  
+  logger.info('Connected to the database');  
 });
 
 app.use('/api', appRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
